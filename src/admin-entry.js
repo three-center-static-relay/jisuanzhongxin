94ec446b0bed5cbeaedf895a293a16656dd292bd
menimall8ewel <2860518767@qq.com>
2026-08-17 08:24:31 +0800

    chore: restore normal production admin entry after P24c

--- src/admin-entry.js
+++ src/admin-entry.js
@@ -1,4 +1,4 @@
-import app,{CenterGate} from "./production-entry-baidu-p24c-sdk039-e2e.js";
+import app,{CenterGate} from "./production-entry.js";
 import {getAutonomySnapshot,runAutonomySweep} from "./provider-autonomy.js";
 export {CenterGate};
 
